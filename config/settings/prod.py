106cfb47e3371edb69858f9be384111559ee0093
yanis-san <barbarayanis14@gmail.com>
2026-03-31 14:03:26 +0100

    prod

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,7 +25,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
